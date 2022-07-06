d03f5bea557ffaba8b8e436b3c8d69dcb52822ee
6six7sven <sushant.679p@gmail.com>
2022-07-07 02:28:11 +0530

    board and square

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-
+import Board from "./components/Board";
 
 // this is not html syntax its akhchually react syntax
 // a component must return a single element 
 // <> is basically react fragment (empty element)
 const App = () => {
-  return (
-    <>
-      <h1>welcome to react </h1>
-      <p> minimal oh damn thingy?</p>
-      </>
-  );
+	return (
+		<div>
+			<h1> Tic Tac Toe </h1>
+			<Board></Board>
+		</div>
+	);
 };
 
 export default App;
@@ -22,7 +22,7 @@ export default App;
 /* 
 export default () => (
   <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
+	<h1>Welcome to React Vite Micro App!</h1>
+	<p>Hard to get more minimal than this React app.</p>
   </>
 ); */
