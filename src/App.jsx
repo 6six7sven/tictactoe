c39043a35537cd64dcae1b3332e4b6be3d86529b
6six7sven <sushant.679p@gmail.com>
2022-07-07 01:52:09 +0530

    git init

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+// this is not html syntax its akhchually react syntax
+// a component must return a single element 
+// <> is basically react fragment (empty element)
+const App = () => {
+  return (
+    <>
+      <h1>welcome to react </h1>
+      <p> minimal oh damn thingy?</p>
+      </>
+  );
+};
+
+export default App;
+
+
+
+
+
+/* 
+export default () => (
+  <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+  </>
+); */
